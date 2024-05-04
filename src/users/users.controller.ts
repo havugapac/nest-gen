90ef65964c7c0d923f070c609dc9362a2247cdb9
havugapac <45178578+havugapac@users.noreply.github.com>
2024-05-05 00:00:44 +0200

    Adding Validation and Prisma

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ParseIntPipe, ValidationPipe } from '@nestjs/common';
+import { query } from 'express';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService : UsersService){}
+
+    // @Get() //GET /users
+    // findAll(){
+    //  return []
+    // }
+
+    // FindAll with query parameters
+    @Get() //GET /users
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
+     return this.usersService.findAll(role)
+    }
+
+    @Get(':id') //GET /users/:id
+    findOne(@Param('id', ParseIntPipe) id:number){
+     return this.usersService.findOne(id)
+    }
+
+    @Post() //POST //Users
+    create(@Body(ValidationPipe) createUserDto: CreateUserDto){
+      return this.usersService.create(createUserDto)
+    }
+
+    @Patch(':id') //PATCH //Users
+    update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateUserDto: UpdateUserDto){
+        return this.usersService.update(id, updateUserDto)
+    }
+
+    @Delete(':id') //DELETE //Users
+    delete(@Param('id', ParseIntPipe) id:number){
+        return this.usersService.delete(id)
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,87 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id" : 1,
+            "name" : "TestUser1",
+            "email" : "user1@test.com",
+            "role" : "ADMIN"
+        },
+        {
+            "id" : 2,
+            "name" : "TestUser2",
+            "email" : "user2@test.com",
+            "role" : "INTERN"
+        },
+        {
+            "id" : 3,
+            "name" : "TestUser3",
+            "email" : "user3@test.com",
+            "role" : "ENGINEER"
+        },
+        {
+            "id" : 4,
+            "name" : "TestUser4",
+            "email" : "user4@test.com",
+            "role" : "ENGINEER"
+        }
+    ]
+
+    findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
+        if(role){
+            const rolesArray = this.users.filter(user => user.role === role)
+
+            if(rolesArray.length === 0) throw new NotFoundException("Invalid Role Passed")
+
+            return rolesArray
+        }
+        return this.users
+        
+    } 
+
+    findOne(id: number){
+        const user = this.users.find(user => user.id === id)
+
+        if(!user) throw new NotFoundException("This User Doesn't Exist Please")
+
+        return user
+    }
+
+    create(createUserDto: CreateUserDto){
+
+        const usersByHighestId = [...this.users].sort((a,b) => b.id - a.id)
+        const newUser = {
+            id : usersByHighestId[0].id+1 , ...createUserDto
+        }
+
+        this.users.push(newUser)
+
+        return newUser
+    }
+
+    update(id: number, updateUserDto : UpdateUserDto){
+        this.users = this.users.map(user => {
+            if(user.id === id){
+                return { ...user, ...updateUserDto}
+            }
+
+            return user
+        })
+
+        return this.findOne(id)
+    }
+
+    delete(id: number){
+        const removedUser = this.findOne(id)
+
+        this.users = this.users.filter(user => user.id !== id)
+
+        return removedUser
+
+    }
+}
